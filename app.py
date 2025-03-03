2dd12b7db6434cd9225d6c468d76221194fc8014
delsharest <fdhlalfrz@gmail.com>
2025-03-03 09:57:46 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,31 +7,37 @@ app.secret_key = 'your_secret_key'  # Ganti dengan kunci rahasia yang lebih kuat
 def home():
     return render_template('index.html')
 
-@app.route('/question1', methods=['GET', 'POST'])
+@app.route('/question1', methods=['POST'])
 def question1():
-    if request.method == 'POST':
-        answer = request.form.get('answer')
-        if answer == "Iyaa, gw di siniii":  # Memperbarui untuk mencocokkan nilai baru
-            return render_template('question2.html')  # Arahkan ke pertanyaan kedua
-        else:
-            flash('Kenapa jawab ngga? Berarti bukan Yas, ya?')  # Menyimpan pesan
-            return redirect(url_for('home'))  # Kembali ke home
-    return render_template('question1.html')
+    answer = request.form.get('answer')
+    if answer == "Iyaa, gw di siniii":
+        return redirect(url_for('question2'))  # Arahkan ke pertanyaan kedua
+    else:
+        flash('Kenapa jawab ngga? Berarti bukan Yas, ya?')
+        return redirect(url_for('home'))  # Kembali ke home
 
 @app.route('/question2', methods=['GET', 'POST'])
 def question2():
     if request.method == 'POST':
         destination = request.form.get('destination')
         if destination == 'Bandung, kuliah nih sibuk':
-            return render_template('bandung_reminders.html', show_popup=True)
+            return redirect(url_for('bandung_reminders'))
         elif destination == 'Sukabumi, libur cuyyy':
-            return render_template('sukabumi_reminders.html', show_popup=True)
+            return redirect(url_for('sukabumi_reminders'))
     return render_template('question2.html')
 
-@app.route('/final_message', methods=['GET', 'POST'])
+@app.route('/bandung_reminders')
+def bandung_reminders():
+    return render_template('bandung_reminders.html', show_popup=True)
+
+@app.route('/sukabumi_reminders')
+def sukabumi_reminders():
+    return render_template('sukabumi_reminders.html', show_popup=True)
+
+@app.route('/final_message')
 def final_message():
-    answer1 = request.args.get('answer1')
-    answer2 = request.args.get('answer2')
+    answer1 = request.args.get('answer1', 'Tidak diketahui')
+    answer2 = request.args.get('answer2', 'Tidak diketahui')
     return render_template('final_message.html', answer1=answer1, answer2=answer2)
 
 if __name__ == '__main__':
